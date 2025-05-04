d69bb2f784e7258d481e59b9d47a5df1b868d46a
Lucas Kells <kellucd@dunwoody.edu>
2025-05-04 11:57:59 -0500

    added db instance

---
+++ modules/database/main.tf
@@ -0,0 +1,16 @@
+resource "aws_db_instance" "lk-super-db" {
+    allocated_storage = 20
+    storage_type = "gp2"
+    engine = "mysql"
+    engine_version = "5.7"
+    instance_class = "db.t2.micro"
+   /*  name = "lk-superdb"
+    username = "XXXXX"
+    password = "XXXXXXXXXXXXX" */
+    parameter_group_name = "default.mysql5.7"
+    publicly_accessible = true
+    skip_final_snapshot = true
+    storage_encrypted = true
+    
+
+}

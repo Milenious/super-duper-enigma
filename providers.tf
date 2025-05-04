362c9950e9f430bae19728838538c3d5006cfc6e
Lucas Kells <kellucd@dunwoody.edu>
2025-05-04 11:59:13 -0500

    fixed fmt

--- providers.tf
+++ providers.tf
@@ -1,12 +1,12 @@
 // Primary region provider for deployment
 
 provider "aws" {
-    region = "us-east-1"
-    alias = "primary"
+  region = "us-east-1"
+  alias  = "primary"
 }
-  
+
 // Secondary region provider for deployment
 provider "aws" {
-    region = "us-west-2"
-    alias = "secondary"
+  region = "us-west-2"
+  alias  = "secondary"
 }
